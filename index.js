9ffb97f95f83815dde98f1487ac8bf9992d551ed
Neil T <neiltz711@gmail.com>
2019-09-17 19:11:58 -0400

    basic example

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'neil',
+  password: '*****',
+  database: 'acme'
+});
+
+db.connect();
+
+app.get('/users', (req, res) => {
+  const sql =
+    'SELECT first_name, last_name, email FROM users ORDER BY last_name';
+
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+
+    res.send(result);
+  });
+});
+
+app.listen(5000, () => console.log('Server started'));
